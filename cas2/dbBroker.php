3020a343021eefecf2f8a5a7beaad84eff141149
Alex <aleksapantic.2000@gmail.com>
2024-12-28 02:50:40 +0100

    Revert "Just a tiny bit better formatting"
    This reverts commit d6563612aed7a6db271decf88568a89a2fd7363a.

--- cas2/dbBroker.php
+++ cas2/dbBroker.php
@@ -4,11 +4,21 @@
     $db = "kolokvijumi";
     $user = "root";
     $pass = "";
-    
+
+
+
     $conn = new mysqli($host, $user, $pass, $db);
 
     if($conn->errno) {
         exit("Neuspesna konekcija: greska".$conn->error.", errkod".$conn->error);
     }
+
+
+
+
+
+
+
+
 
 ?>
